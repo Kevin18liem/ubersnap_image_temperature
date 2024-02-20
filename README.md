bb8761b679a6a3be2a79500ac31df4b90f3c95f1
kevin_desk_ajari <kevin@ajari.app>
2024-02-20 23:33:03 +0700

    Add Image Temperature Adjustment Script

--- README.md
+++ README.md
@@ -1,2 +1,56 @@
 # ubersnap_image_temperature
+
 This is the repo contains solution for Ubersnap Software Developer Challenge
+
+## Purpose
+
+The script `adjust_image_temp.py` can adjust the temperature of input image and save the result to image. The script only accepts **JPEG**  type image. The script make it possible to make the image color look cooler or warmer. See the results of temperature adjustment in folder `demo_images`.
+
+## How to Use
+
+### Installation
+You can create python virtual environment using venv or conda.
+
+e.g Using venv to create the environment
+
+```console
+username@user:~$ python3.10 -m venv .venv
+username@user:~$ source .venv/bin/activate
+(venv) username@user:~$ pip install -r requirements.txt
+```
+
+### Arguments
+```console
+(venv) username@user:~$ python adjust_image_temp.py -h
+usage: adjust_image_temp.py [-h] -i INPUT_FILE -o OUTPUT_PATH -t TEMPERATURE
+
+Argument to Use the Image Temperature Scripts
+
+options:
+  -h, --help            show this help message and exit
+  -i INPUT_FILE, --input_file INPUT_FILE
+                        Name of Input Image with JPEG type
+  -o OUTPUT_PATH, --output_path OUTPUT_PATH
+                        Name of Output Path for the Image with JPEG type
+  -t TEMPERATURE, --temperature TEMPERATURE
+                        Temperature Adjustment for the image, positive for
+                        warmer and negative for for cooler
+```
+
+### Template to run the script
+```console
+(venv) username@user:~$ python adjust_image_temp.py --input_file [image_file_path] --output_path [image_output_name] -t [decimal number]
+```
+
+### Example 
+```console
+(venv) username@user:~$ python adjust_image_temp.py --input_file demo_images/input/demo_2.jpg --output_path demo_images/output/demo_2_cooler_out.jpeg -t -4
+Successfully Adjust the Image Temperature to Cooler
+
+(venv) username@user:~$ python adjust_image_temp.py --input_file demo_images/input/demo_2.jpg --output_path demo_images/output/demo_2_warmer_out.jpeg -t 4
+Successfully Adjust the Image Temperature to Warmer
+```
+
+### Explanation
+
+The script is implemented based on Grayworld Assumption. Assuming that we have a good distribution of colors in our scene, the average reflected color should be the color of the light. If the light source is assumed to be white, we know how much the whitepoint should be moved in the color cube. Check further on [this link](https://pippin.gimp.org/image-processing/chapter-automaticadjustments.html) for more details about the algorithm.
---
+++ adjust_image_temp.py
@@ -0,0 +1,88 @@
+"""Script to Adjust Image Temperature"""
+
+import argparse
+import os
+import cv2
+import numpy as np
+from PIL import Image
+
+
+def validate_input(input_file_path: str, output_path: str) -> bool:
+    """Function to validate the input for the script"""
+
+    # Check is the input image exists or not
+    if not os.path.exists(input_file_path):
+        return False
+
+    try:
+        # Check is the input_image JPEG or Not
+        input_image = Image.open(input_file_path)
+        # Check is the output_path valid with extension for JPEG type
+        return input_image.format == 'JPEG' and output_path.lower().endswith(('.jpeg', 'jpg'))
+    except IOError:
+        return False
+
+
+def adjust_image_temperature(input_image_path: str, output_path: str, temperature: float) -> None:
+    """
+    Function to adjust the image temperature and save it to output path. 
+    Utilize the Grayworld Assumption
+    """
+    try:
+        input_image = cv2.imread(input_image_path)
+        # Convert the image from BGR (Blue, Green, Red) to (Lightness, a*, b*)
+        result_image = cv2.cvtColor(input_image, cv2.COLOR_BGR2LAB)
+
+        # take the average of a* value from every pixel
+        avg_a = np.average(result_image[:, :, 1])
+        # take the average of b* value from every pixel
+        avg_b = np.average(result_image[:, :, 2])
+
+        # Adjust the temperature by changing every corresponding value of a and b,
+        # make the adjustment by cover all the pixels / points of the image
+        result_image[:, :, 1] = result_image[:, :, 1] - \
+            ((avg_a - 128) * (result_image[:, :, 0] / 255.0) * -temperature)
+        result_image[:, :, 2] = result_image[:, :, 2] - \
+            ((avg_b - 128) * (result_image[:, :, 0] / 255.0) * -temperature)
+
+        # Convert the image back from LAB to BGR
+        result_image = cv2.cvtColor(result_image, cv2.COLOR_LAB2BGR)
+        cv2.imwrite(output_path, result_image)
+
+    except Exception as e:
+        print("Failed to process image because : ", str(e))
+
+
+def main():
+    """Main Function to Running The Script"""
+    parser = argparse.ArgumentParser(
+        description="Argument to Use the Image Temperature Scripts")
+    parser.add_argument("-i", "--input_file",
+                        help="Name of Input Image with JPEG type", required=True)
+    parser.add_argument("-o", "--output_path",
+                        help="Name of Output Path for the Image with JPEG type", required=True)
+    parser.add_argument("-t", "--temperature",
+                        help="Temperature Adjustment for the image, \
+                            positive for warmer and negative for for cooler",
+                        required=True, type=float)
+
+    args = parser.parse_args()
+
+    if not validate_input(args.input_file, args.output_path):
+        print("Invalid  Input")
+    else:
+        adjust_image_temperature(
+            args.input_file, args.output_path, args.temperature)
+
+        temperature_message = "Warmer"
+        if args.temperature < 0:
+            temperature_message = "Cooler"
+        print(f'Successfully Adjust the Image Temperature to {temperature_message}')
+
+if __name__ == "__main__":
+    main()
+
+# It expresses color as three values:
+# L* for perceptual lightness and a* and b* for the four unique colors of human vision: red, green, blue and yellow.
+# Need to explain why convert it to LAB
+# Grayworld assumption
